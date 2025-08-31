fd836fce8cdf336a9bc857a06fd29fb68c8b4048
jhedev96 <jhejentkmc@gmail.com>
2025-09-01 05:50:07 +0700

    update rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-import babel from "rollup-plugin-babel";
-import { terser } from "rollup-plugin-terser";
+import babel from "@rollup/plugin-babel";
+import { terser } from "@rollup/plugin-terser";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
@@ -34,6 +34,7 @@ export default {
     resolve(),
     commonjs(),
     babel({
+      babelHelpers: "bundled",
       exclude: "node_modules/**"
     }),
     terser()
